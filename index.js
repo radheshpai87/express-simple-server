a1e5fa0597dbe556cd3b310fc25adf73221d50ea
Radhesh Pai <radhesh.ratnakar.s87@kalvium.community>
2025-04-29 10:26:47 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 8080;
+
+app.use(cors());
 
 let obj = {
     name: 'John Doe',
